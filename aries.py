e9736ec0f499a560cefa2ffa1d79d3ceb385584e
2-propanol <maiden635@gmail.com>
2019-04-23 18:25:35 +0900

    add: 例外処理の追加
    is_connectedは常時最新ではないことに注意

--- aries.py
+++ aries.py
@@ -22,6 +22,11 @@ class Aries:
     """
     ARIES 3軸ステージを制御するクラス
 
+    Parameters
+    ----------
+    is_connected : bool
+        telnetが生きていればTrue、死んでいればFalseを返す。
+
     Attributes
     ----------
     `host` : str
@@ -30,41 +35,82 @@ class Aries:
         ARIESの接続に使うポート番号。デフォルトは 12321。
     """
 
+    _EOL = b"\r\n"
+    is_connected = False
+
     def __init__(self, host="192.168.1.20", port=12321):
         """
         コンストラクタ。telnetへ接続開始。
+
+        10秒経って接続されなかったらタイムアウトする。
+        同期処理のため、接続されるか10秒経つまで待機する。
         """
 
-        self.tn = Telnet(host, port)
+        try:
+            self.tn = Telnet(host, port, 10)
+        except Exception as err:
+            # ホストが見つからなかったときやタイムアウトが起こったときを想定
+            # 他のエラーも握り潰しているためリファクタリングの余地あり
+            # （ConnectionRefusedError は観測済み）
+            print(">>> ", err)
+        else:
+            self.is_connected = True
 
     def __del__(self):
         """
         デストラクタ。telnetから切断。
 
         telnetプロセスがpython終了後も残るのを防ぐため。
-        明示的に呼び出す必要はない。
+        `del aries`のように明示的に呼び出す必要はない。
         """
 
-        self.tn.close()
+        try:
+            self.tn.close()
+            self.is_connected = False
+        except AttributeError:
+            # そもそもtelnetに接続されなかったときの例外
+            pass
+
+    def raw_command(self, cmd):
+        """
+        "RPS1/4/90000/1"のような従来のコマンドをそのまま使用する。
+        同期処理のため返答があるまで待機する。
+
+        Parameters
+        ----------
+        cmd : str
+            対象の果物のマスタID。
+
+        Returns
+        -------
+        stdout : str
+            コマンド実行結果。
+
+        `WAP`(アプリケーション接続数設定 書換), `TRS`(トリガ信号出力選択)などの
+        このクラスで実装されていないコマンドも実行可能。
+        """
+
+        # self.tn.write(bytes(cmd))
+        # self.tn.write(cmd.encode)
+        # self.tn.write(b"RPS3/2/9000/0\r\n")
+        self.tn.write(cmd)
+        # self.tn.write(self._EOL)
 
 
 def main():
-    print("tn opening")
-
+    # aries = Aries("192.168.1.20", 12321)
     aries = Aries()
 
-    print("tn opened")
+    if aries.is_connected:
+        print("tn opened")
+    else:
+        return 1
 
-    # tn.write(b"RPS1/2/9000/1\r\n")
+    aries.raw_command(b"RPS2/2/9000/1\r\n")
     # tn.write(b"ORG3/3/1\r\n")
-
-    print("tn wrote")
 
     # print(tn.read_all().decode('ascii'))
 
-    print("tn closing")
-    del aries
-    print("tn closed")
     return 0
 
 
